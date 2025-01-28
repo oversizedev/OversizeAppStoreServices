3a101714a40aaeebab33720760052ea3e5f9382c
Alexander Romanov <aromanov07@gmail.com>
2025-01-28 20:26:40 +0300

    Added new codes

--- Sources/OversizeAppStoreServices/Models/Typs/TerritoryRegion.swift
+++ Sources/OversizeAppStoreServices/Models/Typs/TerritoryRegion.swift
@@ -19,35 +19,50 @@ public enum TerritoryRegion: String, Sendable, CaseIterable, Identifiable {
 
     public init?(territoryCode: TerritoryCode) {
         switch territoryCode {
-        case .usa, .can:
+        // North America
+        case .usa, .can, .umi:
             self = .unitedStatesAndCanada
 
+        // Europe (including associated territories)
         case .gbr, .fra, .deu, .ita, .esp, .nld, .bel, .swe, .nor, .dnk,
              .fin, .irl, .che, .aut, .grc, .prt, .pol, .cze, .hun, .rou,
              .svk, .svn, .bgr, .hrv, .est, .lva, .ltu, .isl, .mlt, .lux,
              .cyp, .ukr, .blr, .mda, .and, .mco, .smr, .vat, .lie, .alb,
-             .bih, .mkd, .mne, .rus, .srb, .tur, .xks:
+             .bih, .mkd, .mne, .rus, .srb, .xks,
+             // European territories and dependencies
+             .ggy, .jey, .imn, .fro, .gib, .shn:
             self = .europe
 
+        // Africa, Middle East, and India
         case .are, .sau, .isr, .egy, .ind, .zaf, .ken, .nga, .gha, .mar,
              .tur, .qat, .bhr, .omn, .kwt, .jor, .lbn, .irq, .pak, .afg,
-             .dza, .ago, .arm, .aze, .ben, .bwa, .bfa, .cmr, .cpv, .tcd,
-             .cog, .cod, .civ, .gab, .gmb, .geo, .gnb, .gnq, .lbr, .swz,
-             .lby, .mdg, .mwi, .mli, .mrt, .mus, .moz, .nam, .ner, .rwa,
-             .stp, .sen, .syc, .sle, .tza, .tun, .uga, .yem, .zmb, .zwe:
+             .dza, .ago, .arm, .aze, .ben, .bwa, .bfa, .bdi, .cmr, .cpv,
+             .tcd, .com, .cog, .cod, .civ, .dji, .gab, .gmb, .geo, .gnb,
+             .gnq, .eri, .swz, .eth, .lby, .mdg, .mwi, .mli, .mrt, .mus,
+             .moz, .nam, .ner, .rwa, .stp, .sen, .syc, .sle, .som, .ssd,
+             .tza, .tgo, .tun, .uga, .yem, .zmb, .zwe,
+             // Associated territories
+             .reu, .myt:
             self = .africaMiddleEastIndia
 
+        // Latin America and Caribbean
         case .mex, .bra, .arg, .chl, .col, .per, .ven, .ury, .pry, .bol,
              .ecu, .guy, .sur, .pan, .cri, .slv, .gtm, .hnd, .nic, .blz,
              .hti, .dom, .cub, .jam, .tto, .bhs, .brb, .atg, .vct, .grd,
              .dma, .kna, .lca, .aia, .msr, .tca, .vgb, .cym, .guf, .flk,
-             .spm, .bmu:
+             .spm, .bmu,
+             // Additional Caribbean territories
+             .abw, .cuw, .sxm, .bes, .glp, .mtq, .pri, .vir, .ant:
             self = .latinAmericaCaribbean
 
+        // Asia Pacific
         case .chn, .jpn, .kor, .aus, .nzl, .sgp, .hkg, .twn, .mys, .idn,
              .tha, .phl, .vnm, .khm, .lao, .mmr, .brn, .fji, .png, .plw,
              .fsm, .nru, .wsm, .ton, .vut, .slb, .mdv, .lka, .btn, .mng,
-             .kaz, .kgz, .mac, .npl, .tjk, .tkm, .uzb:
+             .kaz, .kgz, .mac, .npl, .tjk, .tkm, .uzb,
+             // Additional Pacific territories
+             .asm, .cok, .cxr, .gum, .kir, .mhl, .mnp, .ncl, .nfk, .niu,
+             .nru, .pyf, .tuv, .wlf:
             self = .asiaPacific
 
         default:
